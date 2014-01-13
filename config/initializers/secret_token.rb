40908d36761bbf52584da707867978a479b501b9
Obi <andrew.obrien1@gmail.com>
2014-01-14 10:49:43 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberappkitrailsCrm::Application.config.secret_key_base = '3f47c6b198004d2a6661105d5b8fdce6dcbcdb919b5ff1e4e2056860da8669a2d644fae7b2fc874406f6fea98983ae7ee5b5e1f8ec53c22bd0a102e92cf2e14b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmberappkitrailsCrm::Application.config.session_store :cookie_store, key: '_emberappkitrails_crm_session'
