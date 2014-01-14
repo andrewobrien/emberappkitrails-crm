41d086096c581131c28957a9f83cbddf5931f6e8
Obi <andrew.obrien1@gmail.com>
2014-01-14 11:42:03 +1100

    bootstrap ember-appkit-rails  && add twitter bootstrap

---
+++ README.md
@@ -0,0 +1,3 @@
+## Using Ember-appkit-rails to do Ember CRM tutorial:
+- [Ember-appkit-rails](https://github.com/dockyard/ember-appkit-rails)
+- [Ember CRM tutorial](http://programmingarehard.com/2013/12/18/ember-crm.html)
---
+++ config/application.js
@@ -0,0 +1,11 @@
+//= require jquery
+//= require environment
+//= require ember-appkit
+//= require_self
+//= require adapter
+//= require router
+//= require_tree ../app
+//= require_tree ./initializers
+//= require bootstrap
+
+window.App = require('app').default.create();
