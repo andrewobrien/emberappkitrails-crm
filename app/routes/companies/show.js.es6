7ecd43a7f8c7395fd3ec26b763b048ab59b7f837
Obi <andrew.obrien1@gmail.com>
2014-01-16 18:16:36 +1100

    update companies templates

--- app/routes/companies/show.js.es6
+++ app/routes/companies/show.js.es6
@@ -1,5 +1,3 @@
-import Index from 'app/routes/companies/index'
-
 export default Ember.Route.extend({
   model: function(params) {
     return this.store.find('company', params.company_id);
--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -1,6 +1,6 @@
 class CompanySerializer < ActiveModel::Serializer
   attributes :id, :name
 
-  # embed :ids, include: true
-  # has_many :people
+  embed :ids, include: true
+  has_many :people
 end
